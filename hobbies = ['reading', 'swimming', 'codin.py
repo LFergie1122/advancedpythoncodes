fcd7202596af7cabdb9b17edc85409f7fdcd52cc
LFergie1122 <london.ferguson777@gmail.com>
2025-03-09 14:23:20 -0500

    hobbies_set

---
+++ hobbies = ['reading', 'swimming', 'codin.py
@@ -0,0 +1,4 @@
+hobbies = ['reading', 'swimming', 'coding', 'painting', 'painting']
+hobbies_set = set(hobbies)
+print("original list:", hobbies_set)
+print("set with duplicates removed:", hobbies_set)
