2bbb200756329758a9d337241022460afb29f40b
LFergie1122 <london.ferguson777@gmail.com>
2025-03-09 14:51:27 -0500

    favorite_mvies

---
+++ favorite_movies = {'The Dark Knight', 'T.py
@@ -0,0 +1,3 @@
+favorite_movies = {'The Dark Knight', 'Taken', 'Hypnotic', 'Watcher', 'Salt'}
+for movies in favorite_movies:
+    print(movies)
--- hobbies = ['reading', 'swimming', 'codin.py
+++ hobbies = ['reading', 'swimming', 'codin.py
@@ -1,4 +1,3 @@
 hobbies = ['reading', 'swimming', 'coding', 'painting', 'painting']
 hobbies_set = set(hobbies)
 print("original list:", hobbies_set)
-print("set with duplicates removed:", hobbies_set)
